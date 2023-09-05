1814f0e211588e32683494adf6aeaa9b90041618
Vishnu Balakrishnan <vishnu@hercules12.bc.rzg.mpg.de>
2023-09-05 15:51:09 +0200

    Added a check before running filtool, will always run filtool on the last fil file. Fix for TRAPUM sub-banding pipeline bug

--- data_copy_scheduler.sh
+++ data_copy_scheduler.sh
@@ -4,17 +4,20 @@ USER="vishnu"
 HOST="archive"
 #DATA_DIR="/p/MFR/MEERKAT/TRAPUM/TRAPUM_517/TRAPUM/SCI-20180923-MK-04/J1740-5340A/2020-10-20-15:40:16/1284/"
 #DATA_DIR="/p/MFR/MEERKAT/TRAPUM/TRAPUM_517/TRAPUM/SCI-20180923-MK-04/J1740-5340A/2020-11-05-10:13:37/1284/"
-DATA_DIR="/p/MFR/MEERKAT/TRAPUM/TRAPUM_518/TRAPUM/SCI-20180923-MK-04/J1740-5340A/2020-11-05-10:13:37/1284/"
-OUTPUT_DIR="/hercules/scratch/vishnu/TRAPUM/observations/NGC6397/20201105/"
+#DATA_DIR="/p/MFR/MEERKAT/TRAPUM/TRAPUM_518/TRAPUM/SCI-20180923-MK-04/J1740-5340A/2020-11-05-10:13:37/1284/"
+DATA_DIR="/p/MFR/MEERKAT/TRAPUM/TRAPUM_511/TRAPUM/SCI-20180923-MK-04/NGC_6441/2020-07-09-17:33:18/1284/"
+#OUTPUT_DIR="/hercules/scratch/vishnu/TRAPUM/observations/NGC6397/20201105/"
+OUTPUT_DIR="/hercules/scratch/vishnu/TRAPUM/observations/NGC6441/20200709/"
 filtool_img="/u/vishnu/singularity_images/pulsarx_latest.sif"
 filtool_cmd="filtool -t 12 --telescope meerkat -z zdot --cont"
-cluster="NGC6397"
-epoch="20201105"
+cluster="NGC6441"
+epoch="20200709"
 code_dir="/hercules/scratch/vishnu/SLURM_PULSARMINER/"
 #pm_config_file="NGC6397_20201020_accel_search.config"
 pm_config_file="NGC6397_20201105_accel_search_only.config"
-PROCESSED_FILE="${code_dir}processed_dirs_NGC6397_20201105.txt"
+PROCESSED_FILE="${code_dir}processed_dirs_NGC6441_20200709.txt"
 
+mkdir -p $OUTPUT_DIR
 
 # Create the processed file if it doesn't exist
 if [ ! -f ${PROCESSED_FILE} ]; then
@@ -26,22 +29,17 @@ SUBDIRS=$(ssh ${USER}@${HOST} "find ${DATA_DIR} -mindepth 1 -maxdepth 1 -type d 
 
 # Iterate over each subdirectory
 for SUBDIR in ${SUBDIRS}
-do
+do  
+    if [[ "$SUBDIR" != "${DATA_DIR}cfbf00012" ]]; then
+        continue
+    fi
     # Check if this subdirectory has been processed
     if grep -Fxq "${SUBDIR}" ${PROCESSED_FILE}
     then
     echo "${SUBDIR} has already been processed. Skipping."
     continue
     fi
- 
-    # Check if the file size on the remote server is less than 100MB
-    #REMOTE_SIZE=$(ssh ${USER}@${HOST} du -sB1 "${SUBDIR}" | cut -f1)
 
-    #if [ ${REMOTE_SIZE} -lt 104857600 ]
-    #then
-    #    echo "${SUBDIR} is less than 100MB. Skipping."
-    #    continue
-    #fi
 
     echo "Starting rsync for ${SUBDIR}"
 
@@ -55,17 +53,28 @@ do
         continue
     fi
     
-    
     beam=$(basename ${SUBDIR})
     cd ${OUTPUT_DIR}${beam}
-    # Run filtool
-    singularity exec -H $HOME:/home1 -B /hercules/:/hercules/ $filtool_img $filtool_cmd -o ${cluster}_${epoch}_${beam}_zdot -f ${OUTPUT_DIR}${beam}/*.fil
 
-    # Check if filtool was successful
-    if [ $? -ne 0 ]
-    then
-        echo "Filtool failed for ${cluster}_${epoch}_${beam}. Moving to the next subdirectory."
-        continue
+    # Count number of .fil files. If there are more than one, sort them and get the last one. Fixing a TRAPUM sub-banding pipeline bug.
+    num_files=$(ls *.fil 2>/dev/null | wc -l)
+
+    # Check condition and execute accordingly
+    if [ "$num_files" -gt 1 ]; then
+        # Sort files and get the last one
+        last_file=$(ls -v *.fil | tail -n 1)
+        singularity exec -H $HOME:/home1 -B /hercules/:/hercules/ $filtool_img $filtool_cmd -o ${cluster}_${epoch}_${beam}_zdot -f ${OUTPUT_DIR}${beam}/$last_file
+        if [ $? -ne 0 ]; then
+            echo "Filtool failed for ${cluster}_${epoch}_${beam}. Moving to the next subdirectory."
+            continue
+        fi
+    elif [ "$num_files" -eq 1 ]; then
+        single_file=$(ls *.fil)
+        singularity exec -H $HOME:/home1 -B /hercules/:/hercules/ $filtool_img $filtool_cmd -o ${cluster}_${epoch}_${beam}_zdot -f ${OUTPUT_DIR}${beam}/$single_file
+        if [ $? -ne 0 ]; then
+            echo "Filtool failed for ${cluster}_${epoch}_${beam}. Moving to the next subdirectory."
+            continue
+        fi
     fi
 
     # Create a symlink to the fil file
@@ -81,28 +90,28 @@ do
     # Start SLURM Pulsarminer inside a screen session on the filtoled file
     screen -S SLURM_PULSARMINER_${cluster}_${epoch}_${beam} -dm bash  -c "cd ${code_dir} && ./LAUNCH_SLURM_PULSARMINER.sh -o ${cluster}_${epoch}_${beam}.fil -p $pm_config_file"
 
-    # Check if pulsarminer start was successful
+    ## Check if pulsarminer start was successful
     if [ $? -ne 0 ]
     then
-        echo "Pulsarminer start failed for ${cluster}_${epoch}_${beam}. Moving to the next subdirectory."
-        continue
+       echo "Pulsarminer start failed for ${cluster}_${epoch}_${beam}. Moving to the next subdirectory."
+       continue
     fi
 
-    #echo "Started Pulsarminer for ${cluster}_${epoch}_${beam}_zdot_01.fil. Sleeping for 1 hours..."
-    echo "Started Pulsarminer for ${cluster}_${epoch}_${beam}_zdot_01.fil. Sleeping for 40 minutes..."
+    echo "Started Pulsarminer for ${cluster}_${epoch}_${beam}_zdot_01.fil. Sleeping for 2 hours..."
+    #echo "Started Pulsarminer for ${cluster}_${epoch}_${beam}_zdot_01.fil. Sleeping for 40 minutes..."
     # Mark this directory as processed
     echo ${SUBDIR} >> ${PROCESSED_FILE}
 
     # Delete pre-filtoled files
     cd ${OUTPUT_DIR}${beam}
     for file in *.fil; do
-        if [ "$file" != ${cluster}_${epoch}_${beam}_zdot_01.fil ]; then
-            rm -f $file
-        fi
+       if [ "$file" != ${cluster}_${epoch}_${beam}_zdot_01.fil ]; then
+           rm -f $file
+       fi
     done
 
     # Sleep for 2 hours 
-    sleep 40m
+    sleep 2h
 
 done
 
