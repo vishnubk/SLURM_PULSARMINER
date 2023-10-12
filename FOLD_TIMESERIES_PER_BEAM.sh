4fc5e63ddd0aa5ac304f124b78ee2f346ffa6fcd
Vishnu Balakrishnan <vishnu@hercules11.bc.rzg.mpg.de>
2023-10-12 13:51:08 +0200

    added timeseries folding and candidate shortlister

---
+++ FOLD_TIMESERIES_PER_BEAM.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+sing_image=$1
+data_dir=$2
+code_dir=$3
+working_dir=$4
+previous_results=$5
+rawdata_basename=$6
+fold_script_file=$7
+raw_fold_script_file=$8
+ncpus=$9
+pm_config=${10}
+fold_timeseries=${11}
+rfifind_mask=${previous_results}/01_RFIFIND
+search_results=${previous_results}/03_DEDISPERSION
+fold_results=${previous_results}/05_FOLDING
+birdie_directory=${previous_results}/02_BIRDIES
+
+#Cleaning up any prior runs
+rm -rf $working_dir
+
+
+mkdir -p $working_dir
+#Copy birdie directory
+rsync -Pav $birdie_directory $working_dir
+
+unique_accel_files=$(grep -oP '(?<=-accelfile )\S+' $fold_script_file | sort | uniq)
+unique_accel_files=$(echo "$unique_accel_files" | grep '03_DEDISPERSION' | sort -u | sed 's/.*03_DEDISPERSION\///')
+disk_path_accel_files=$(echo "$unique_accel_files" | sed "s#^#$search_results/#")
+
+unique_timeseries_files=$(grep -o '/[^ ]*\.dat' $fold_script_file | sort | uniq)
+unique_timeseries_files=$(echo "$unique_timeseries_files" | grep '03_DEDISPERSION' | sort -u | sed 's/.*03_DEDISPERSION\///')
+disk_path_timeseries_files=$(echo "$unique_timeseries_files" | sed "s#^#$search_results/#")
+
+
+echo "Creating directory structure in working directory"
+echo "$disk_path_accel_files" | tr ' ' '\n' | xargs -I {} dirname {} | sort -u | while IFS= read -r dir; do
+  filename=$(echo "$dir" | grep '03_DEDISPERSION' | sort -u | sed 's/.*03_DEDISPERSION/03_DEDISPERSION/')
+  mkdir -p "$working_dir/$filename"
+done
+
+
+echo "Copying accel files to working directory"
+echo "$disk_path_accel_files" | tr ' ' '\n' | while IFS= read -r file; do
+   filename=$(echo "$file" | grep '03_DEDISPERSION' | sort -u | sed 's/.*03_DEDISPERSION/03_DEDISPERSION/')
+   dir=$(dirname "$filename")
+   truncated_filename="${file%_ACCEL_*}"
+   rsync -Pav --exclude='*.dat' ${truncated_filename}* "$working_dir/$dir/"
+done
+
+echo "Copying timeseries files to working directory"
+if [ $fold_timeseries == 1 ]; then
+   echo "$disk_path_timeseries_files" | tr ' ' '\n' | while IFS= read -r file; do
+   filename=$(echo "$file" | grep '03_DEDISPERSION' | sort -u | sed 's/.*03_DEDISPERSION/03_DEDISPERSION/')
+   dir=$(dirname "$filename")
+   rsync -Pav $file "$working_dir/$dir/"
+   rsync -Pav "${file%.dat}.inf" "$working_dir/$dir/"
+   done
+
+fi
+
+
+## Copy the raw data, rfifind mask, search and sifting results to the working directory
+echo "Copying config file"
+rsync -Pav ${code_dir}/${pm_config} $working_dir
+echo "Copying rfifind mask"
+rsync -Pav $rfifind_mask $working_dir
+
+#
+mkdir -p ${working_dir}/05_FOLDING_TIMESERIES/$rawdata_basename
+mkdir -p ${working_dir}/05_FOLDING/$rawdata_basename
+
+echo "Copying folding code"
+rsync -Pav $code_dir/pm_run_multithread ${working_dir}/05_FOLDING_TIMESERIES/$rawdata_basename/
+rsync -Pav $code_dir/$fold_script_file ${working_dir}/05_FOLDING_TIMESERIES/$rawdata_basename/
+rsync -Pav $code_dir/$raw_fold_script_file ${working_dir}/05_FOLDING_TIMESERIES/$rawdata_basename/
+rsync -Pav $fold_results/$rawdata_basename/candidates.csv ${working_dir}/05_FOLDING/$rawdata_basename/
+
+basename_fold_script_file=$(basename "$fold_script_file")
+fold_script_file=${working_dir}/05_FOLDING_TIMESERIES/$rawdata_basename/${basename_fold_script_file}
+
+basename_raw_fold_script_file=$(basename "$raw_fold_script_file")
+raw_fold_script_file=${working_dir}/05_FOLDING_TIMESERIES/$rawdata_basename/${basename_raw_fold_script_file}
+
+cd ${working_dir}/05_FOLDING_TIMESERIES/$rawdata_basename/
+singularity exec -H $HOME:/home1 -B $data_dir:$data_dir $sing_image python pm_run_multithread -cmdfile $basename_fold_script_file -ncpus $ncpus
+
+#-l flag is set when raw data is already folded. The code then acts as a shortlister else it will create a separate raw candidates file that needs to be folded
+singularity exec -H $HOME:/home1 -B $data_dir:$data_dir $sing_image python ${code_dir}/calculate_alpha_and_shortlist_cands.py -pfds ${working_dir}/05_FOLDING_TIMESERIES/$rawdata_basename/ -s $raw_fold_script_file -l
+
+
+
+rsync -Pav --exclude='*.pfd*' --exclude='LOG*' --exclude='pm_run_multithread' ${working_dir}/05_FOLDING_TIMESERIES $previous_results
+rsync -Pav ${working_dir}/05_FOLDING $previous_results
+
+#Clean Up
+rm -rf $working_dir
---
+++ LAUNCH_CANDIDATE_SHORTLISTER.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+cluster="NGC6397"
+epoch="20201105"
+code_dir="/hercules/scratch/vishnu/SLURM_PULSARMINER"
+pm_config="NGC6397_accel_search_gpu_only_accel200.config"
+
+for beam in cfbf{00000..00287}; do
+#beam="cfbf00071"
+rawdata_basename="${cluster}_${epoch}_${beam}"
+
+if [ -f "${cluster}_${epoch}_${beam}.fil" ]; then
+    echo file "${cluster}_${epoch}_${beam}.fil" exists
+    slurm_job_file="slurm_jobs_${cluster}_${epoch}_${beam}.sh"
+    if [ -f $slurm_job_file ]; then
+        slurm_job_file_no_ext="${slurm_job_file%.*}"
+        head -n 5 $slurm_job_file > ${slurm_job_file_no_ext}_candidate_shortlist.sh
+        echo "slurm_ids=\"\"" >> "${slurm_job_file_no_ext}_candidate_shortlist.sh"
+        echo "" >> "${slurm_job_file_no_ext}_candidate_shortlist.sh"
+        grep "dedisp=" "$slurm_job_file" > temp_dedisp_lines.txt
+        while IFS= read -r line; do
+            echo "$line" >> "${slurm_job_file_no_ext}_candidate_shortlist.sh"
+            echo "" >> "${slurm_job_file_no_ext}_candidate_shortlist.sh"
+            echo 'slurmids="$slurmids:$dedisp"' >> "${slurm_job_file_no_ext}_candidate_shortlist.sh"
+            echo "" >> "${slurm_job_file_no_ext}_candidate_shortlist.sh"
+        done < temp_dedisp_lines.txt
+
+        # Remove the temporary file
+        rm temp_dedisp_lines.txt
+        awk '/sift_and_create_fold_command_file=/{print}' "$slurm_job_file" | while read -r line; do 
+            echo "$line" | grep -q -- "--dependency=afterok$slurmids" && echo "$line" || echo "$line" | sed "s/--parsable/--parsable --dependency=afterok\$slurmids/"
+        done >> "${slurm_job_file_no_ext}_candidate_shortlist.sh"
+
+
+        #awk '/sift_and_create_fold_command_file=/{print}' "$slurm_job_file" | sed 's/--parsable/--parsable --dependency=afterok\l$slurmids/' >> "${slurm_job_file_no_ext}_candidate_shortlist.sh"
+        echo "" >> "${slurm_job_file_no_ext}_candidate_shortlist.sh"
+        echo "sbatch --dependency=afterok:\$sift_and_create_fold_command_file --job-name=timeseries_folds --output=\$logs/${cluster}_${epoch}_${beam}_fold_timeseries_get_alpha.out --error=\$logs/${cluster}_${epoch}_${beam}_fold_timeseries_get_alpha.err -t 4:00:00 -p short.q --mem=220GB --cpus-per-task=48 --wrap=\"${code_dir}/FOLD_TIMESERIES_PER_BEAM.sh /u/vishnu/singularity_images/presto_gpu.sif /hercules ${code_dir} /tmp/${cluster}/${epoch}/${beam} ${code_dir}/$cluster/$epoch/$beam $rawdata_basename $cluster/$epoch/$beam/05_FOLDING/$rawdata_basename/script_fold_timeseries.txt $cluster/$epoch/$beam/05_FOLDING/$rawdata_basename/script_fold.txt 48 ${pm_config} 1\"" >> "${slurm_job_file_no_ext}_candidate_shortlist.sh"
+
+        exit 0
+
+    else
+        echo file $slurm_job_file does not exist. Run LAUNCH_SLURM_PULSARMINER.sh on the observation file to generate the slurm job file.
+    fi
+
+else
+    echo file "${cluster}_${epoch}_${beam}.fil" does not exist. You need to copy over the filterbank file and run filtool.
+fi
+done
